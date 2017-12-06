c128d45fcf4e1941039cb47255a3f571d75f46f5
yanchhuong <yanchhuo_ng@yahoo.com>
2017-12-06 15:21:15 +0700

    ComponentScan

--- src/main/java/com/code/demo/Application.java
+++ src/main/java/com/code/demo/Application.java
@@ -19,6 +19,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.code")
