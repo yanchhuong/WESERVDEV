5364d56907d082140113875a58040c5d5bc28fd8
yanchhuong <yanchhuo_ng@yahoo.com>
2017-12-07 18:24:21 +0700

    jdbc test

---
+++ src/main/java/com/code/model/RoleBusinessBeanOut_R001.java
@@ -0,0 +1,62 @@
+package com.code.model;
+public class RoleBusinessBeanOut_R001 {
+	private long rid;
+	private String username;
+	private String role;
+	private String regdate;
+	private String sdate;
+	private String edate;
+	private String title;
+	private String usercd;
+	
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+	public String getUsercd() {
+		return usercd;
+	}
+	public void setUsercd(String usercd) {
+		this.usercd = usercd;
+	}
+	public long getRid() {
+		return rid;
+	}
+	public void setRid(long rid) {
+		this.rid = rid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getSdate() {
+		return sdate;
+	}
+	public void setSdate(String sdate) {
+		this.sdate = sdate;
+	}
+	public String getEdate() {
+		return edate;
+	}
+	public void setEdate(String edate) {
+		this.edate = edate;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+}
