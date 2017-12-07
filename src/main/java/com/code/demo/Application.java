87c5a3b546f12c8015e2615c8f4bd9663c240f82
yanchhuong <yanchhuo_ng@yahoo.com>
2017-12-07 19:39:20 +0700

    chat2

--- src/main/java/com/code/demo/Application.java
+++ src/main/java/com/code/demo/Application.java
@@ -19,8 +19,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-
+@EntityScan(basePackages = "com.code.model")
 @SpringBootApplication
 @ComponentScan("com.code")
 public class Application extends SpringBootServletInitializer {
