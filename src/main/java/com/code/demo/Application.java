6c3cdd942ae3f2eebe87da3d6a26cfd80b11a7db
yanchhuong <yanchhuo_ng@yahoo.com>
2017-12-07 15:23:00 +0700

    fix Unable to start embedded container

--- src/main/java/com/code/demo/Application.java
+++ src/main/java/com/code/demo/Application.java
@@ -33,5 +33,7 @@ public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+    
 
 }
