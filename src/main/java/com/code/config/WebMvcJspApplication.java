28be9d7309705213f35d4854f94527c07b9b4de6
yanchhuong <yanchhuo_ng@yahoo.com>
2018-01-22 18:30:30 +0700

    CONFIG 220118

--- src/main/java/com/code/config/WebMvcJspApplication.java
+++ src/main/java/com/code/config/WebMvcJspApplication.java
@@ -15,29 +15,45 @@
  */
 package com.code.config;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-
-import com.code.model.StorageProperties;
-import com.code.service.StorageService;
-
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@ComponentScan("com.code")
+import com.code.service.StorageService;
+import com.code.model.StorageProperties;
+/**
+ * @author Adarsh Kumar
+ * @author $LastChangedBy: Adarsh Kumar$
+ * @version $Revision: 0001 $, $Date:: 1/1/10 0:00 AM#$
+ * @Espark @copyright all right reserve
+ */
+
+@Configuration
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.code")
+/*@EntityScan(basePackages = "com.code.model")*/
+@EnableJpaRepositories(basePackages = "com.code.dao")
+@PropertySource("classpath:application.properties")
 @EnableConfigurationProperties(StorageProperties.class)
 
 public class WebMvcJspApplication extends SpringBootServletInitializer{
 	private static  Logger LOGGER =  LoggerFactory.getLogger(WebMvcJspApplication.class);
-
+	private int maxUploadSizeInMb = 10 * 1024 * 1024; // 10 MB
 
 	
 	@Override
@@ -61,4 +77,22 @@ public class WebMvcJspApplication extends SpringBootServletInitializer{
             storageService.init();
 		};
 	}
+	 
+	 //Tomcat large file upload connection reset
+	    //http://www.mkyong.com/spring/spring-file-upload-and-connection-reset-issue/
+	    @Bean
+	    public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
+
+	        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+
+	        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+	            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+	                //-1 means unlimited
+	                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(maxUploadSizeInMb);
+	            }
+	        });
+
+	        return tomcat;
+
+	    }
 }
