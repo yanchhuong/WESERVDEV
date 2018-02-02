adae6d2cd44e329012012188b9a4c77b03c39315
yanchhuong <yanchhuo_ng@yahoo.com>
2018-02-02 11:47:20 +0700

    Conf-----

--- src/main/java/com/code/model/CategoryBean_R001.java
+++ src/main/java/com/code/model/CategoryBean_R001.java
@@ -1,14 +1,13 @@
 package com.code.model;
 import java.io.Serializable;
 import javax.persistence.Column;
-import javax.persistence.Entity;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
-@Entity(name = "category")
-@Table(name="category")
+/*@Entity(name = "category")
+@Table(name="category")*/
 public class CategoryBean_R001 implements Serializable { 
 	private static final long serialVersionUID = 1L;
 	@Id
