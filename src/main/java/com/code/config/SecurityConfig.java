b1a2fdc64de82553a381d7f9c2c656c08eeb3d90
yanchhuong <yanchhuo_ng@yahoo.com>
2018-02-02 15:41:40 +0700

    security config

--- src/main/java/com/code/config/SecurityConfig.java
+++ src/main/java/com/code/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.code.config;
 
 import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,52 +10,94 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import com.code.service.UserService;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	DataSource dataSource;
+	
+	@Autowired
+	private UserService iUserDao;
+	SecurityConfig(UserService iUserDao){
+		this.iUserDao=iUserDao;
+	}
 
 	@Autowired
 	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication().dataSource(dataSource)
-				.passwordEncoder(passwordEncoder())
+		        .passwordEncoder(passwordEncoder())
 				.usersByUsernameQuery("select username,password, enabled from users where username=?")
 				.authoritiesByUsernameQuery("select username, role from user_roles where username=?");
-		        
 
 	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-
+		
+		
+                 
 				// control by log in for page
-				.antMatchers("/chatting")
-				.access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN') or hasRole('ROLE_EMPLOYEE')")
+				.antMatchers("/chatting").access("hasAnyRole('ROLE_USER','ROLE_ADMIN','ROLE_EMPLOYEE')")
 
 				.antMatchers("/users").access("hasRole('ROLE_ADMIN')")
 				.antMatchers("/users/add").access("hasRole('ROLE_ADMIN')")
 				.antMatchers("/users/**/update").access("hasRole('ROLE_ADMIN')")
 				.antMatchers("/users/**/delete").access("hasRole('ROLE_ADMIN')")
-
+				
+				.antMatchers("/post").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN') or hasRole('ROLE_EMPLOYEE')")
+				
+				.antMatchers("/admin").access("hasRole('ROLE_ADMIN')")
+				.antMatchers("/users/add").access("hasRole('ROLE_ADMIN')")
+				
+				.antMatchers("/users/**/update").access("hasRole('ROLE_ADMIN')")
 				.anyRequest().permitAll()
+				
+		        .antMatchers("/login*","/signin/**","/signup/**").permitAll()
+		   /*.and() 
+		      .authorizeRequests().antMatchers("/rest/**").authenticated()//  authenticationEntryPoint(new RESTAuthenticationEntryPoint()
+           */				
 		   .and()
-		   		.formLogin().loginPage("/login")
+		   		.formLogin()
+		   		.loginPage("/login")
+		   		.successHandler(new SuccessLoginHandler(iUserDao))//.defaultSuccessUrl("/")
+		   		.failureHandler(new CustomLoginFailureHandler())//.failureUrl("/login")
 		   		.usernameParameter("username")
 				.passwordParameter("password")
-				
 			.and()
 				.logout().logoutSuccessUrl("/login?logout")
-				
+			.and()
+			 	.rememberMe()
+			 	.rememberMeCookieName("example-app-remember-me")
+		        .tokenRepository(persistentTokenRepository())
+		        .tokenValiditySeconds(24 * 60 * 60)
 			.and()
 				.exceptionHandling().accessDeniedPage("/403")
-			.and().csrf();
+				.accessDeniedHandler(accessDeniedHandler())
+			.and().csrf()
+			.csrfTokenRepository(new CookieCsrfTokenRepository()) // setting token
+			;
+		// add iframe work
+		http.headers().frameOptions().disable();
 	}
+	
 	@Bean
 	public PasswordEncoder passwordEncoder(){
 		PasswordEncoder encoder = new BCryptPasswordEncoder();
 		return encoder;
 	}
-
+	@Bean
+	public AccessDeniedHandler accessDeniedHandler(){
+	    return new CustomAccessDeniedHandler();
+	}
+	
+	@Bean
+	  public PersistentTokenRepository persistentTokenRepository() {
+	      JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+	      repo.setDataSource(dataSource);
+	      return repo;
+	 }
 }
