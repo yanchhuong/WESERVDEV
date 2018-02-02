05ca1604d4420054a35c9320513bd51c9013a1f0
yanchhuong <yanchhuo_ng@yahoo.com>
2018-02-02 11:54:00 +0700

    config----

---
+++ src/main/java/com/code/service/impl/FileImageServiceImpl.java
@@ -0,0 +1,52 @@
+package com.code.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.code.dao.IFileImageDao;
+import com.code.model.FileUploadBean;
+import com.code.service.IFileImageService;
+@Service(value="fileService")
+public class FileImageServiceImpl implements IFileImageService{
+ 
+	IFileImageDao iFileImageDao ;
+	@Autowired
+	public FileImageServiceImpl(IFileImageDao iFileImageDao){
+		this.iFileImageDao=iFileImageDao;
+	}
+	@Override
+	public List<FileUploadBean> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveFileUploadBean(FileUploadBean fileUploadBean) {
+		this.iFileImageDao.insertNew(fileUploadBean);
+	}
+
+	@Override
+	public FileUploadBean findOne(long fileUploadBeanId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(String filename) {
+		this.iFileImageDao.remove(filename);
+	}
+	@Override
+	public long getPIDCount() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+	@Override
+	public void insertNew(FileUploadBean fileUploadBean) {
+		this.iFileImageDao.insertNew(fileUploadBean);
+		
+	}
+
+
+}
